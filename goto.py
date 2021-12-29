bbb91e418888335cd70664ab16832ac05cf7e087
EnderC00kiez <49785023+EnderC00kiez@users.noreply.github.com>
2021-12-29 09:23:58 -0500

    Update goto.py

--- goto.py
+++ goto.py
@@ -1,1 +1,7 @@
-print("go to https://github.com/eanderso2/shiny-octo-engine")
+import webbrowser
+print("Do you want to go to https://github.com/eanderso2/shiny-octo-engine [Y/n]")
+if input().lower() == "n" or "no":
+  webbrowser.open("https://github.com/eanderso2/shiny-octo-engine")
+else:
+  print("Okay.")
+  exit()
